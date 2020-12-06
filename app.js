bf32d4b9593544554b0dd202b87f8ae37e70d97d
slackoO <slemmerlaszlo@gmail.com>
2020-12-07 00:43:01 +0100

    Added cards and basic draw functionality

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let card = document.querySelector('.card')
-    let timerId = setInterval(addListElement, 1000)
+const drawBtn = document.querySelector('#drawButton')
+const cardFront = document.querySelector('#cardFront')
+const cardBack = document.querySelector('#cardBack')
+let current = 0
+let cardFrontSrcRoot = "img/fronts/FRONT_R"
+let cardBackSrcRoot = "img/backs/BACK_R"
+let imageExtension = ".jpg"
+const numberOfCards = 22
 
-    function addListElement() {
-        card.appendChild(document.createElement("li"))
-    }
-})
+drawBtn.addEventListener('click',start)
+
+function start() {
+    let next = Math.floor(1 + Math.random() * numberOfCards)
+    cardBack.src = cardBackSrcRoot + next + imageExtension
+    current = next
+    drawBtn.removeEventListener('click', start)
+    drawBtn.addEventListener('click', drawCard)
+}
+
+function drawCard() {
+    let next = Math.floor(1 + Math.random() * numberOfCards)
+    cardBack.src = cardBackSrcRoot + next + imageExtension
+    cardFront.src = cardFrontSrcRoot + current + imageExtension
+    current = next
+}
